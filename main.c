dfd9322840743ca1bb526e327dd424c9032f060e
DatLem0n <sahki232@gmail.com>
2023-09-24 00:55:00 +0300

    fixed for linux and finished week 6

--- main.c
+++ main.c
@@ -5,11 +5,13 @@
 float lampotila(uint16_t rekisteri, float kerroin);
 float kosteus(uint16_t rekisteri);
 float valoisuus(uint16_t rekisteri);
+uint32_t ilmanpaine(uint8_t xlsb, uint8_t lsb, uint8_t msb);
 
 int main() {
     printf("lampotila on :%.02f\n", lampotila(0b0011001000000000, 0.03125));
     printf("kosteus on :%.02f\n", kosteus(0b1000000000000000));
     printf("valoisuus on :%.02f\n", valoisuus(0b0110110000110101));
+    printf("paine on :%u\n", ilmanpaine(0b11011110, 0b10111100, 0b00011000));
     return 0;
 }
 
@@ -26,4 +28,11 @@ float valoisuus(uint16_t rekisteri){
     uint16_t exponent = rekisteri >> 12;
     uint16_t fractional = rekisteri & 0b0000111111111111;
     return 0.01 * pow(2, exponent) * fractional;
+}
+
+uint32_t ilmanpaine(uint8_t xlsb, uint8_t lsb, uint8_t msb){
+    uint32_t rekisteri = (0 | msb) << 8;
+    rekisteri = (rekisteri | lsb) << 8;
+    rekisteri = (rekisteri | xlsb);
+    return rekisteri >> 4;
 }
